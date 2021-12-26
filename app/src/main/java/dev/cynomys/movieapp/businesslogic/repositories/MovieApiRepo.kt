73e72869e1c390f687cf5b12da94ec544b0b5ef3
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-26 17:35:33 -0600

    Added page parameter en getMovieMethod

--- app/src/main/java/dev/cynomys/movieapp/businesslogic/repositories/MovieApiRepo.kt
+++ app/src/main/java/dev/cynomys/movieapp/businesslogic/repositories/MovieApiRepo.kt
@@ -1,7 +1,9 @@
 package dev.cynomys.movieapp.businesslogic.repositories
 
+import dev.cynomys.movieapp.model.FullMovie
 import dev.cynomys.movieapp.model.MovieListResponse
 
 interface MovieApiRepo {
-    suspend fun getMovieList(): MovieListResponse
+    suspend fun getMovieList(page: Int): MovieListResponse
+    suspend fun getmovie(id: Int): FullMovie
 }
--- app/src/main/java/dev/cynomys/movieapp/businesslogic/repositories/MovieApiRepoImpl.kt
+++ app/src/main/java/dev/cynomys/movieapp/businesslogic/repositories/MovieApiRepoImpl.kt
@@ -1,8 +1,13 @@
 package dev.cynomys.movieapp.businesslogic.repositories
 
 import dev.cynomys.movieapp.businesslogic.api.MovieApiService
+import dev.cynomys.movieapp.model.FullMovie
 import dev.cynomys.movieapp.model.MovieListResponse
+import javax.inject.Inject
 
-class MovieApiRepoImpl constructor(private val apiService: MovieApiService): MovieApiRepo {
-    override suspend fun getMovieList(): MovieListResponse = apiService.getMovieList()
+class MovieApiRepoImpl @Inject constructor(private val apiService: MovieApiService) : MovieApiRepo {
+    override suspend fun getMovieList(page: Int): MovieListResponse =
+        apiService.getMovieList(page = page)
+
+    override suspend fun getmovie(id: Int): FullMovie = apiService.getMovie(id)
 }

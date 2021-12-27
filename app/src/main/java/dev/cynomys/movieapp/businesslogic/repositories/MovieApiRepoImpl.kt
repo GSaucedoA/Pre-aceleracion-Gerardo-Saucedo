b383ec9e18c2642b55b7f54fd33b928a7be8910d
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-26 18:59:10 -0600

    Implemented main-safety

--- app/src/main/java/dev/cynomys/movieapp/businesslogic/repositories/MovieApiRepoImpl.kt
+++ app/src/main/java/dev/cynomys/movieapp/businesslogic/repositories/MovieApiRepoImpl.kt
@@ -3,11 +3,16 @@ package dev.cynomys.movieapp.businesslogic.repositories
 import dev.cynomys.movieapp.businesslogic.api.MovieApiService
 import dev.cynomys.movieapp.model.FullMovie
 import dev.cynomys.movieapp.model.MovieListResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MovieApiRepoImpl @Inject constructor(private val apiService: MovieApiService) : MovieApiRepo {
     override suspend fun getMovieList(page: Int): MovieListResponse =
-        apiService.getMovieList(page = page)
+        withContext(Dispatchers.IO) {
+            apiService.getMovieList(page = page)
+        }
 
-    override suspend fun getmovie(id: Int): FullMovie = apiService.getMovie(id)
+    override suspend fun getmovie(id: Int): FullMovie =
+        withContext(Dispatchers.IO) { apiService.getMovie(id) }
 }
